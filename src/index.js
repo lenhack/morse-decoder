b6330372d88cbfcd2c9a5d4504e062bc4359f337
Lenhack <lenhack1@gmail.com>
2022-01-31 00:05:23 +0200

    task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,62 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    while(expr.length % 10 != 0){
+        expr += '0';
+    }
+
+    console.log(expr);
+
+    const temp = [];
+    for(let i = 0; i < expr.length; i++){
+        temp[i] = expr[i];
+    }
+
+    const arrLeters = [];
+    while(temp.length >= 10){
+        arrLeters.push(temp.splice(0, 10));
+    }
+
+    const doubleStr = [];
+    for(let i = 0; i < arrLeters.length; i++){
+        doubleStr[i] = [];
+        while(arrLeters[i].length >= 2){
+            doubleStr[i].push(arrLeters[i].splice(0, 2).join(''));
+        }
+    }
+
+    const dashesAndDotsArr = [];
+    for(let i = 0; i < doubleStr.length; i++){
+        dashesAndDotsArr[i] = [];
+        for(let y = 0; y < doubleStr[i].length; y++){
+            if(doubleStr[i][y] === '10'){
+                dashesAndDotsArr[i][y] = '.';
+            } else if(doubleStr[i][y] === '11') {
+                dashesAndDotsArr[i][y] = '-';
+            } else if(doubleStr[i][y] === '**'){
+                dashesAndDotsArr[i][y] = ' ';
+            } else {
+                dashesAndDotsArr[i][y] = '';
+            }
+        }
+    }
+
+    const strDashes = [];
+    for(let i = 0; i < dashesAndDotsArr.length; i++){
+        strDashes.push(dashesAndDotsArr[i].join(''));
+    }
+
+
+    let result = '';
+    for(let i = 0; i < strDashes.length; i++){
+        if(MORSE_TABLE[strDashes[i]] === undefined){
+            result += ' ';
+        } else {
+            result = result + MORSE_TABLE[strDashes[i]];
+        }
+    }
+    return result;
 }
 
 module.exports = {
